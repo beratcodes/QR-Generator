4316e06f6a152117fc13e2f7c6fddfa328720a91
beratcodes <159455724+beratcodes@users.noreply.github.com>
2025-12-29 22:09:22 +0300

    Variable name is changed.

--- qr.py
+++ qr.py
@@ -11,7 +11,7 @@ from pyqrcode import QRCode
 
 # func
 def createQR():
-    url = qr_label.get()
+    url = qr_entry.get()
 
     if url:
         qr_url = pyqrcode.create(url)
@@ -28,12 +28,12 @@ app_window.resizable(0,0)
 
 
 url_label = tk.Label(app_window, text="URL Girin: ")
-qr_label = tk.Entry(app_window, width=40)
+qr_entry = tk.Entry(app_window, width=40)
 qr_code_button = tk.Button(app_window, text="QR Generate", command=createQR)
 status_label = tk.Label(app_window, text="")
 
 url_label.grid(row=0,column=0,padx=10,pady=10)
-qr_label.grid(row=0,column=1,padx=10,pady=10)
+qr_entry.grid(row=0,column=1,padx=10,pady=10)
 qr_code_button.grid(row=1,column=0,columnspan=2,padx=10,pady=10)
 status_label.grid(row=2,column=0,columnspan=2,padx=10,pady=10)
 
