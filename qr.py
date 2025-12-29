f847228e19ea82bcbce5f97d7bf7d4ca5fa17010
beratcodes <159455724+beratcodes@users.noreply.github.com>
2025-12-29 16:25:12 +0300

    Variable name is changed.

--- qr.py
+++ qr.py
@@ -13,7 +13,7 @@ def createQR():
 
         if qr_url:
             qr_url.svg(file_dialog, scale=8)
-            state_label.config(text="QR Oluşturuldu ve Kaydedildi.")
+            status_label.config(text="QR Oluşturuldu ve Kaydedildi.")
 
 # GUI
 app_window = tk.Tk()
@@ -24,11 +24,11 @@ app_window.resizable(0,0)
 url_label = tk.Label(app_window, text="URL Girin: ")
 qr_label = tk.Entry(app_window, width=40)
 qr_code_button = tk.Button(app_window, text="QR Generate", command=createQR)
-state_label = tk.Label(app_window, text="")
+status_label = tk.Label(app_window, text="")
 
 url_label.grid(row=0,column=0,padx=10,pady=10)
 qr_label.grid(row=0,column=1,padx=10,pady=10)
 qr_code_button.grid(row=1,column=0,columnspan=2,padx=10,pady=10)
-state_label.grid(row=2,column=0,columnspan=2,padx=10,pady=10)
+status_label.grid(row=2,column=0,columnspan=2,padx=10,pady=10)
 
 app_window.mainloop()
